e2bc303351669df841a439f64724f852801e7462
Shabhaizafar <zafarshabhai03.com>
2024-08-05 19:38:36 +0530

    New

---
+++ External.js
@@ -0,0 +1,39 @@
+//main Screen
+// document.write("<br>This is External.");
+// document.write("<h1>JS</h1>");
+
+
+
+// //Console Sceen 
+// console.log("External");
+
+
+
+
+/*
+internal JS: 
+Bio Data : 
+
+(main sceen)
+    first name : sdg
+    last name : dfgh
+    Gender : male
+    Hobbies : sdfg,werty
+    Age : 12
+
+
+*/
+
+/*
+External JS: 
+Bio Data : 
+
+(console sceen)
+    first name : sdg
+    last name : dfgh
+    Gender : male
+    Hobbies : sdfg,werty
+    Age : 12
+
+
+*/
