379a10345347919514164c1d2429961fab3c00ad
Shabhai Zafar <zafarshabhai03@gmail.com>
2024-09-01 18:14:00 +0530

    Variables

---
+++ Variable/Variables.js
@@ -0,0 +1,69 @@
+// what is a Variable ?
+
+// int a = 12;
+// float f = 12.3f;
+
+
+// var ,let, const     ==== int float string char ..........
+
+// how to Declare a Variable ? 
+// 1) var 
+
+// var a=12;
+// var a = 3.14;
+// var a = "Zafar";
+// console.log(a);
+
+
+// 2) let 
+// let a = 12;
+// let a = 3.4;
+// let a = "Zafar";
+// console.log(a);
+
+// 3) const 
+// const a = 12;
+// const a = 3.1;
+// const a = "Zafar";
+// console.log(a);
+
+
+// Diference Between let,var,const : 
+
+// var a = 12;
+// a = 100;
+// var a = 1000;
+// console.log(a);
+
+// let b = 13;
+// b = 200;
+// let b = 2000; //error
+// console.log(b);
+
+// const c = 14;
+// c = 300;//error 
+// const c = 1;//error
+// console.log(c);
+////////////////////////////////////
+// Rule of Variables Declaration :
+
+
+// 1) Variable Name Must be Unique.
+// var a = 12;
+// var a = 1234567890;
+
+// 2) Variable Name Must be Meaningful.
+
+
+// 3) we  cann't   use keyword as a Variable name.
+        // var var = 2;  invalid //error
+
+// 4) we  cann't Start Variable name with Numerical Numbers.
+        // var 1num = 12; //error
+
+// 5)  we  cann't Use Special Char in a Variable name.    1) _  2) $
+// var _ = 1;
+// var dfgh@ = 1;
+
+// 6)  cann't use white space in a Variable name ;
+    // var num 1 = 12; 
