20f4ecb5b42115ca0c3637554c0534e9fc8248b9
Shabhai Zafar <zafarshabhai03@gmail.com>
2024-09-02 19:29:47 +0530

    Methods

---
+++ String/StringMethods.js
@@ -0,0 +1,54 @@
+//
+//length : Property 
+
+// charAt
+// charCodeAt
+// concat 
+// includes
+
+var str1 = "           Royal Technosoft pvt Ltd           ";
+console.log(str1);
+
+// str1.endsWith : true/false
+// console.log(str1.endsWith('Z'));
+// console.log(str1.endsWith('Ltd'));
+
+// str1.startsWith
+// console.log(str1.startsWith('Z'));
+// console.log(str1.startsWith('Royal tech'));
+
+
+// str1.indexOf
+// console.log(str1.indexOf('R'));
+// console.log(str1.indexOf('o'));
+
+// str1.lastIndexOf
+// console.log(str1.lastIndexOf('o'));
+
+// str1.replace
+// console.log(str1.replace('Royal','ryl'));
+
+// str1.replaceAll
+// console.log(str1.replaceAll('o','O'));
+
+// str1.toLowerCase
+// console.log(str1.toLowerCase());
+
+// str1.toUpperCase
+// console.log(str1.toUpperCase());
+
+
+// str1.trim
+// console.log(str1.trim());
+
+// str1.trimEnd
+// console.log(str1.trimEnd());
+
+// str1.trimStart
+// console.log(str1.trimStart());
+
+
+console.log(str1);//String
+
+// str1.valueOf
+console.log(str1.valueOf());
