c62c6fdf483cbb3ba9a38e9a1d53b4ab977a5869
Shabhai Zafar <zafarshabhai03@gmail.com>
2024-09-29 18:54:33 +0530

    BasicAll

---
+++ String/String2.js
@@ -0,0 +1,9 @@
+// - Template Strings.  : 
+var fname = "Ajay";
+var lname = "Patel";
+var Age = 13;
+// Hello my name is Raj Shah.and I'm 13 Years Old.
+console.log("Hello my name is",fname,lname,".and I'm", Age,"Years Old.");
+
+console.log(`Hello my name is ${fname} ${lname}.and I'm ${Age} Years Old.`);
+console.log("Hello my name is ${fname} ${lname}.and I'm ${Age} Years Old.");
