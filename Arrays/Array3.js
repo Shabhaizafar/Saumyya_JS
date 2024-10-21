ef44bb481728a4608e1474fd112ee4fbba65f7d8
Shabhaizafar <zafarshabhai03@gmail.com>
2024-10-21 19:47:56 +0530

    Array3

---
+++ Arrays/Array3.js
@@ -0,0 +1,64 @@
+//    - use const for creating arrays.
+// var Arr1 = [1,2,3];
+// console.log(Arr1);
+// Arr1 = [];
+// var Arr1 = [];
+// console.log(Arr1);
+
+// let Arr2 = [11,12,13];
+// console.log(Arr2);
+// let Arr2 = [];
+// Arr2 = [];
+// console.log(Arr2);
+
+// const Arr3 = [45,46,47];
+// console.log(Arr3);
+// const Arr3 = [];
+// Arr3 = [];
+// Arr3.pop();
+// console.log(Arr3);
+
+////////////////////////////////////////////////
+// const Arr1 =[11,12,13,14,15];
+// console.log(Arr1);
+
+//    - For loop.
+// console.log("Basic For Loop : ");
+// for (let i = 0; i < Arr1.length; i++) {
+//     console.log(i,Arr1[i]);
+// }
+//    - While loop in array.
+// console.log("Using While Loop : ");
+// var i =0;
+// while (i<Arr1.length) {
+//     console.log(i,Arr1[i]);
+//     i++;
+// }
+//    - do-while Loop
+// console.log("Using Do-While Loop : ");
+// var i =0;
+// do{
+//     console.log(i,Arr1[i]);
+//     i++;
+// }while (i<Arr1.length);
+
+//    - For of loop.   : value     index: X
+// console.log("Using For of Loop : ");
+// for (const element of Arr1) {
+//     console.log(element);
+// }
+
+//    - For in loop.   index : value
+// console.log("Using For in Loop  : ");
+// for (const key in Arr1) {
+//     console.log(key,Arr1[key]);
+// }
+
+//    - Foreach Loop.  //value
+// console.log("Using Foreach in Loop  : ");
+// Arr1.forEach((element,index) => {
+//     console.log(element,index);
+// });
+
+
+//    - Array destructuring.
